3667e276e5646204452008cac89b3a62a9e2b1b1
jarmoruuth <jarmoruuth@users.noreply.github.com>
2024-11-21 22:19:16 +0200

    physicalMemoryStatus
    physicalMemoryStatus only for PixInsight 1.8.9-3 or later.

--- AutoIntegrateEngine.js
+++ AutoIntegrateEngine.js
@@ -16223,8 +16223,10 @@ this.autointegrateProcessingEngine = function(parent, auto_continue, autocontinu
        console.noteln("--------------------------------------");
        util.addProcessingStepAndStatusInfo("Start processing : " + txt);
        console.writeln("auto_continue : " + auto_continue + ", autocontinue_narrowband : " + autocontinue_narrowband);
-       var memoryStatus = physicalMemoryStatus();
-       console.writeln("Memory status: total bytes: " + memoryStatus.totalBytes + ", available bytes: " + memoryStatus.availableBytes);
+       if (global.pixinsight_version_num >= 108090) {
+            var memoryStatus = physicalMemoryStatus();
+            console.writeln("Memory status: total bytes: " + memoryStatus.totalBytes + ", available bytes: " + memoryStatus.availableBytes);
+       }
  
        if (gui) {
             gui.close_undo_images();
@@ -16737,8 +16739,10 @@ this.autointegrateProcessingEngine = function(parent, auto_continue, autocontinu
        util.addProcessingStepAndStatusInfo("Script completed, time "+(end_time-start_time)/1000+" sec");
        console.noteln("======================================");
 
-       var memoryStatus = physicalMemoryStatus();
-       console.writeln("Memory status: total bytes: " + memoryStatus.totalBytes + ", available bytes: " + memoryStatus.availableBytes);
+       if (global.pixinsight_version_num >= 1080903) {
+            var memoryStatus = physicalMemoryStatus();
+            console.writeln("Memory status: total bytes: " + memoryStatus.totalBytes + ", available bytes: " + memoryStatus.availableBytes);
+       }
 
        console.writeln("--------------------------------------");
        if (global.flowchartData != null) {
