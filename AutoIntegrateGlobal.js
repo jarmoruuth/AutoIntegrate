0213580c744719da546297aad495e3bf2cdbdc0f
jarmoruuth <jarmoruuth@users.noreply.github.com>
2025-04-23 21:39:50 +0300

    v1.75

--- AutoIntegrateGlobal.js
+++ AutoIntegrateGlobal.js
@@ -42,7 +42,7 @@ this.__base__();
 
 /* Following variables are AUTOMATICALLY PROCESSED so do not change format.
  */
-this.autointegrate_version = "AutoIntegrate v1.75 test3";                     // Version, also updated into updates.xri
+this.autointegrate_version = "AutoIntegrate v1.75";                           // Version, also updated into updates.xri
 this.autointegrate_info = "DBE, drizzle options, flat dark calibration";      // For updates.xri
 
 this.autointegrate_version_info = [
