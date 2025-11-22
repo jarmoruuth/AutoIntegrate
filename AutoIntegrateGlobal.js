b10e73e1f9c835b218be5441d0565c063b69ea9e
jarmoruuth <jarmoruuth@users.noreply.github.com>
2025-11-22 15:26:48 +0200

    v1.80

--- AutoIntegrateGlobal.js
+++ AutoIntegrateGlobal.js
@@ -42,7 +42,7 @@ this.__base__();
 
 /* Following variables are AUTOMATICALLY PROCESSED so do not change format.
  */
-this.autointegrate_version = "AutoIntegrate v1.80 test3";   // Version, also updated into updates.xri
+this.autointegrate_version = "AutoIntegrate v1.80";         // Version, also updated into updates.xri
 this.autointegrate_info = "Process Icons";                  // For updates.xri
 
 this.autointegrate_version_info = [
